4aad47fe7345b59bf9e120a1096231c9b73804f5
Gary M. Silva <garymsilva@outlook.com>
2019-10-11 10:09:05 -0300

    refactor(base-project): Start main() & predict some functions

---
+++ src/model/sort.py
@@ -0,0 +1,40 @@
+"""
+registers: list
+compare: function
+"""
+
+def selectionSort(registers, compare):
+    raise NotImplementedError
+#
+
+def insertionSort(registers, compare):
+    raise NotImplementedError
+#
+
+def mergeSort(registers, compare):
+    raise NotImplementedError
+#
+
+def quickSort(registers, compare):
+    raise NotImplementedError
+#
+
+def heapSort(registers, compare):
+    raise NotImplementedError
+#
+
+def introSort(registers, compare):
+    raise NotImplementedError
+#
+
+def timSort(registers, compare):
+    raise NotImplementedError
+#
+
+def smoothSort(registers, compare):
+    raise NotImplementedError
+#
+
+def patienceSort(registers, compare):
+    raise NotImplementedError
+#
